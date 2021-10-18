5992197ed1fafa7a728d0176d6ee876888672aff
Eunsoo Lee <eunsoogi.lee@gmail.com>
2021-10-18 20:48:09 +0900

    Change version to 1.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='dtroslib',
-    version='1.1.0',
+    version='1.2.0',
     author='Hanyang University',
     url='https://github.com/DeepTaskHY/ROS.git',
     packages=setuptools.find_packages(),
